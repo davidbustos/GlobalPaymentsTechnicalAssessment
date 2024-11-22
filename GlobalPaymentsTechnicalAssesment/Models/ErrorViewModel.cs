1c7196cf9da8892540ca38691ad47c997cb7ccce
David Bustos <davidbustosrod@gmail.com>
2024-11-21 23:12:42 -0600

    Initial Commit: Creating .net 8 MVC app for Global Payments Assesment

---
+++ GlobalPaymentsTechnicalAssesment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GlobalPaymentsTechnicalAssesment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
