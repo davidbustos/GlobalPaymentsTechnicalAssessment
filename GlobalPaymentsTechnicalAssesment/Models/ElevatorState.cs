0402b90c9f6514682da497919de7e71309908f8a
David Bustos <davidbustosrod@gmail.com>
2024-11-24 22:49:45 -0600

    Fixes Internal and External Controls issue

--- GlobalPaymentsTechnicalAssesment/Models/ElevatorState.cs
+++ GlobalPaymentsTechnicalAssesment/Models/ElevatorState.cs
@@ -6,7 +6,7 @@
         public string State { get; set; } = "Idle"; // Idle, Moving, Opening, Closing
         public string DoorState { get; set; } = "Closed"; // Closed, Opening, Opened, Closing
         public double Position { get; set; } = 0.0; // Position in meters
-        public double Speed { get; set; } = .5;   // Speed in meters per second
+        public double Speed { get; set; } = 2;   // Speed in meters per second
         public double FloorHeight { get; set; } = 2.5; // Height between floors in meters
         public int TimeToOpenDoors { get; set; } = 1; // Time to open doors in seconds
         public int TimeToCloseDoors { get; set; } = 1; // Time to close doors in seconds
